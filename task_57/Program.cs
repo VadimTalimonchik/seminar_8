23f12a1f4902eb1b2ac1db5687b6a8a5eb5c7617
VadimTalimonchik <tvadmins@gmail.com>
2022-12-23 13:19:35 +0300

    Задача 57

--- task_57/Program.cs
+++ task_57/Program.cs
@@ -16,4 +16,126 @@
 //                     4 встречается 1 раз 
 //                     6 встречается 2 раза
 
-Console.WriteLine("Hello, World!");
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0}	", array[i, j]);
+        }
+        Console.WriteLine(" ");
+    }
+}
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество колонок: ");
+int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine(" ");
+int[,] array = new int[rows, columns];
+array = CreateArray(rows, columns);
+Console.WriteLine(" ");
+PrintArray(array);
+Console.WriteLine(" ");
+
+void DigitCounter(int[,] array)
+{
+    int counter = 0;
+    for (int digit = 0; digit < 10; digit++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (array[i, j] == digit) counter++;
+            }
+        }
+        if (counter == 2 || counter == 3 || counter == 4 )
+        {
+            Console.WriteLine($"Цифра {digit} встретилась {counter} раза");
+        }
+        else
+        {
+            Console.WriteLine($"Цифра {digit} встретилась {counter} раз");
+        }
+        counter = 0;
+    }
+}
+
+DigitCounter(array);
+
+
+// // Вариант 2 - недоделанный
+
+// int[,] CreateArray(int rows, int columns)
+// {
+//     int[,] array = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             array[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+//     return array;
+// }
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write("{0}	", array[i, j]);
+//         }
+//         Console.WriteLine(" ");
+//     }
+// }
+
+// void PrintOneDimArray(int[] array);
+// {
+//     for (int j = 0; j < length; j++)
+//     {
+//         Console.Write(array[j]);
+//     }
+// }
+
+// Console.Write("Enter the number or rows...");
+// int rows = int.Parse(Console.ReadLine()!);
+// Console.Write("Enter the number or columns..");
+// int columns = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(" ");
+// int[,] array = new int[rows, columns];
+// array = CreateArray(rows, columns);
+// Console.WriteLine(" ");
+// PrintArray(array);
+// Console.WriteLine(" ");
+
+
+// int[] OneDimensionArray (int[,] array)
+// {
+//     int element = 0;
+//     int [] OneDimensionArray = new int [(array.GetLength(0))*(array.GetLength(1))];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             element = array[i,j];
+//             OneDimensionArray = element;
+//         }
+//     }
+//     return OneDimensionArray;
+// }
+
+// PrintOneDimArray(OneDimensionArray(array));
