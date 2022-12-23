3dfea3b2ede3363952ab65360d2d94ecf094e641
VadimTalimonchik <tvadmins@gmail.com>
2022-12-23 13:32:49 +0300

    Задача 59+

--- task_59/Program.cs
+++ task_59/Program.cs
@@ -13,110 +13,190 @@
 // 2 2 6
 // 3 4 7
 
-int[,] CreateArray(int rows, int columns)
-{
-    int[,] array = new int[rows, columns];
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            array[i, j] = new Random().Next(0, 11);
-        }
-    }
-    return array;
-}
-void PrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write("{0}	", array[i, j]);
-        }
-        Console.WriteLine(" ");
-    }
-}
-Console.Write("Введите количество строк: ");
-int rows = int.Parse(Console.ReadLine()!);
-Console.Write("Введите количество колонок: ");
-int columns = int.Parse(Console.ReadLine()!);
-Console.WriteLine(" ");
-int[,] array = new int[rows, columns];
-array = CreateArray(rows, columns);
-Console.WriteLine(" ");
+// int[,] CreateArray(int rows, int columns)
+// {
+//     int[,] array = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             array[i, j] = new Random().Next(0, 11);
+//         }
+//     }
+//     return array;
+// }
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write("{0}	", array[i, j]);
+//         }
+//         Console.WriteLine(" ");
+//     }
+// }
+// Console.Write("Введите количество строк: ");
+// int rows = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите количество колонок: ");
+// int columns = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(" ");
+// int[,] array = new int[rows, columns];
+// array = CreateArray(rows, columns);
+// Console.WriteLine(" ");
+// PrintArray(array);
+// Console.WriteLine(" ");
+
+// int CoordinatesMin(int[,] array)
+// {
+//     int min = array[0, 0];
+//     int k = 0;
+//     int p = 0;
+//     int t = 0;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i, j] < min)
+//             {
+//                 min = array[i, j];
+//                 p = i;
+//                 t = j;
+//             }
+//         }
+//     }
+//     Console.WriteLine("MinElement" + " " + min);
+
+//     k = 10 * p + t;
+//     return k;
+// }
+// int k = CoordinatesMin(array);
+// int rowMin = k / 10;
+// int columnMin = k % 10;
+// // PrintArray(DeleteColumn(array, columnMin));
+// PrintArray(DeleteRow(DeleteColumn(array, columnMin), rowMin));
+
+// int[,] DeleteColumn(int[,] array, int columnMin)
+
+// {
+//     int[,] deletedColumn = new int[array.GetLength(0), array.GetLength(1) - 1];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1) - 1; j++)
+//         {
+//             if (j < columnMin)
+//             {
+//                 deletedColumn[i, j] = array[i, j];
+//             }
+//             if (j >= columnMin)
+//             {
+//                 deletedColumn[i, j] = array[i, j + 1];
+//             }
+//         }
+//     }
+//     return deletedColumn;
+// }
+
+
+// int[,] DeleteRow(int[,] array, int rowMin)
+
+// {
+//     int[,] deletedRow = new int[array.GetLength(0)-1, array.GetLength(1)];
+
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         for (int i = 0; i < array.GetLength(0) - 1; i++)
+//         {
+//             if (i < rowMin)
+//             {
+//                 deletedRow[i, j] = array[i, j];
+//             }
+//             if (j >= rowMin)
+//             {
+//                 deletedRow[i, j] = array[i + 1, j];
+//             }
+//         }
+//     }
+//     return deletedRow;
+// }
+
+// Вариант 2
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns);
 PrintArray(array);
-Console.WriteLine(" ");
+Console.WriteLine();
+PrintArray(NewArray(array, FindMinElement(array)));
 
-int CoordinatesMin(int[,] array)
+int[,] GetArray(int m, int n)
 {
-    int min = array[0, 0];
-    int k = 0;
-    int p = 0;
-    int t = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < n; j++)
         {
-            if (array[i, j] < min)
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] FindMinElement(int[,] Matrix) // находим минимальный элемент массива и его индексы
+{
+    int min = Matrix[0, 0];
+    int minI = 0;
+    int minJ = 0;
+    for (int i = 0; i < Matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < Matrix.GetLength(1); j++)
+        {
+            if (Matrix[i, j] < min)
             {
-                min = array[i, j];
-                p = i;
-                t = j;
+                minI = i;
+                minJ = j;
+                min = Matrix[i, j];
             }
         }
     }
-    Console.WriteLine("MinElement" + " " + min);
-
-    k = 10 * p + t;
-    return k;
+    int[] Temporary = { minI, minJ }; // координаты строчки и столбца
+    return Temporary;
 }
-int k = CoordinatesMin(array);
-int rowMin = k / 10;
-int columnMin = k % 10;
-// PrintArray(DeleteColumn(array, columnMin));
-PrintArray(DeleteRow(DeleteColumn(array, columnMin), rowMin));
 
-int[,] DeleteColumn(int[,] array, int columnMin)
-
+int[,] NewArray(int[,] OriginalArray, int[] Coordinati)
 {
-    int[,] deletedColumn = new int[array.GetLength(0), array.GetLength(1) - 1];
-    for (int i = 0; i < array.GetLength(0); i++)
+    int newrow = 0;
+    int newcolumn = 0;
+    int[,] result = new int[OriginalArray.GetLength(0) - 1, OriginalArray.GetLength(1) - 1];
+    for (int i = 0; i < OriginalArray.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        if (i != Coordinati[0])
         {
-            if (j < columnMin)
+            for (int j = 0; j < OriginalArray.GetLength(1); j++)
             {
-                deletedColumn[i, j] = array[i, j];
+                if (j != Coordinati[1])
+                {
+                    result[newrow, newcolumn] = OriginalArray[i, j];
+                    newcolumn++;
+                }
             }
-            if (j >= columnMin)
-            {
-                deletedColumn[i, j] = array[i, j + 1];
-            }
+            newrow++;
+            newcolumn = 0;
         }
     }
-    return deletedColumn;
+    return result;
 }
-
-
-int[,] DeleteRow(int[,] array, int rowMin)
-
-{
-    int[,] deletedRow = new int[array.GetLength(0)-1, array.GetLength(1)];
-
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        for (int i = 0; i < array.GetLength(0) - 1; i++)
-        {
-            if (i < rowMin)
-            {
-                deletedRow[i, j] = array[i, j];
-            }
-            if (j >= rowMin)
-            {
-                deletedRow[i, j] = array[i + 1, j];
-            }
-        }
-    }
-    return deletedRow;
-}
-
